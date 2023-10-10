62427c861d0ba4cae80694269c7b23f1686f2ebf
kareglazkaa <110668807+kareglazkaa@users.noreply.github.com>
2023-10-10 17:26:55 +0300

    Delete src directory

--- src/main/java/com/example/gallery/Utils/Algorithms.java
+++
@@ -1,7 +0,0 @@
-package com.example.gallery.Utils;
-
-public enum Algorithms {
-    LIBRARY,
-    HUFFMAN,
-    VISION
-}
--- src/main/java/com/example/gallery/Utils/FragmentManagerUtil.java
+++
@@ -1,7 +0,0 @@
-package com.example.gallery.Utils;
-
-import androidx.fragment.app.FragmentManager;
-
-public class FragmentManagerUtil {
-    public static FragmentManager fm;
-}
